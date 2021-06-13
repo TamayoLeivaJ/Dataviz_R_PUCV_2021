2b088738e09c5c1f3413503eca07dc2df550519e
TamayoLeivaJ <j.tamayo.leiva@gmail.com>
2021-06-13 00:45:05 -0400

    :building_construction:

--- Dataviz_R_PUCV_2021.R
+++ Dataviz_R_PUCV_2021.R
@@ -1,1 +1,15 @@
-install.packages(c("broom","corrplot","devtools","flextable","FSA","ggbiplot","ggplot2","ggpubr","ggrepel","gridExtra","gtsummary","Hmisc","knitr","patchwork","pheatmap","RColorBrewer","rcompanion","report","rmarckdown","rmarkdown","scales","tidyverse","viridis"))
+cran_packages <- c("broom","corrplot","devtools","flextable","FSA","ggplot2","ggpubr","ggrepel","gridExtra","gtsummary","Hmisc","knitr",
+                   "patchwork","pheatmap","RColorBrewer","rcompanion","report","rmarkdown","rmarkdown","scales","tidyverse","viridis")
+git_packages <- c("vqv/ggbiplot")
+
+# Instalar paquetes CRAN
+.inst <- cran_packages %in% installed.packages()
+if(any(!.inst)) {
+  install.packages(cran_packages[!.inst])
+}
+
+# Instalar paquetes GitHub
+.inst <- git_source %in% installed.packages()
+if(any(!.inst)) {
+  devtools::install_github(git_source[!.inst])
+}
